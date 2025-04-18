ea653d4c9c0acc0b0dd366ac1d1a7ad38a682bd1
elena 3 <123189951+elenapanchoo@users.noreply.github.com>
2025-04-18 17:46:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,21 @@ import os
 from flask import Flask, render_template, request
 from similarity_model import SimilarityModel
 
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)  
-
 app = Flask(__name__, template_folder='templates')
-model: SimilarityModel = SimilarityModel("ontology_descriptions.json")
+model = SimilarityModel("ontology_descriptions.json")  # Initialize your model
 
 @app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
-
 
 @app.route("/check", methods=["POST"])
 def check():
     message = request.form.get("message", "")
     score = model.compare_text(message)
     return render_template("index.html", score=round(score, 4), message=message)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
 
 
